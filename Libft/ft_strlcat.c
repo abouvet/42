2897366ec4489dba95dc0d590846576a2f035023
Alexy BOUVET <abouvet@e1r6p1.42.fr>
2021-12-29 12:42:00 +0100

    29/12 midi

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,21 +6,33 @@
 /*   By: abouvet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 19:31:24 by abouvet           #+#    #+#             */
-/*   Updated: 2021/12/28 20:01:57 by abouvet          ###   ########.fr       */
+/*   Updated: 2021/12/29 10:46:52 by abouvet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
+	unsigned int	s_size;
+	unsigned int	d_size;
+	unsigned int	rslt;
 
+	d_size = 0;
+	while (dest[d_size])
+		d_size++;
+	s_size = 0;
+	while (src[s_size])
+		s_size++;
+	rslt = s_size;
+	if (size <= d_size)
+		rslt += size;
+	else
+		rslt += d_size;
+	s_size = 0;
+	while (src[s_size] && d_size + 1 < size)
+	{
+		dest[d_size] = src[s_size];
+		d_size++;
+		s_size++;
+	}
+	return (rslt);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: abouvet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 17:57:15 by abouvet           #+#    #+#             */
-/*   Updated: 2021/12/28 19:25:15 by abouvet          ###   ########.fr       */
+/*   Updated: 2021/12/29 12:23:17 by abouvet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,23 @@
 # include <string.h>
 # include <stdio.h>
 
-int		ft_isalpha(int car);
-int		ft_isdigit(int car);
-int		ft_isalnum(int car);
-int		ft_isascii(int car);
-int		ft_isprint(int car);
-void	*ft_memset(void *s, int car, size_t size);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, void *src, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-int		ft_toupper(int c);
-int		ft_strlen(char *str);
-int		ft_tolower(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *str);
-char	*ft_strdup(const char *s);
+int					ft_isalpha(int car);
+int					ft_isdigit(int car);
+int					ft_isalnum(int car);
+int					ft_isascii(int car);
+int					ft_isprint(int car);
+void				*ft_memset(void *s, int car, size_t size);
+void				ft_bzero(void *s, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+void				*ft_memmove(void *dest, void *src, size_t n);
+size_t				ft_strlcpy(char *dest, const char *src, size_t size);
+int					ft_toupper(int c);
+int					ft_strlen(char *str);
+int					ft_tolower(int c);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_atoi(const char *str);
+char				*ft_strdup(const char *s);
+unsigned int		ft_strlcat(char *dest, char *src, unsigned int size);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
 #endif
