9b4ce15144527eb463428ced622c71556f56df6b
Alexy BOUVET <abouvet@e1r7p4.42.fr>
2022-01-03 21:15:47 +0100

    3/1

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,12 +6,68 @@
 /*   By: abouvet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 15:16:15 by abouvet           #+#    #+#             */
-/*   Updated: 2021/12/30 15:17:35 by abouvet          ###   ########.fr       */
+/*   Updated: 2022/01/03 21:14:27 by abouvet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
+int	ft_count(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n < 0 || n == 0)
+		i++;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return i;
+}
+
+char	*ft_make(int len, char *tmp, int n)
+{
+	int				i;
+	unsigned int	nb;
+
+	i = 0;
+	if (n < 0)
+	{
+		tmp[0] = '-';
+		nb = -n;
+	}
+	else
+		nb = n;
+	if (nb == 0)
+		tmp[0] = '0';
+	while (nb > 0)
+	{
+		tmp[len - 1 - i] = '0' + (nb % 10);
+		nb /= 10;
+		i++;
+	}
+	tmp[len] = '\0';
+	return (tmp);
+}
+
 char *ft_itoa(int n)
 {
+	int		len;
+	char	*tmp;
+
+	len = ft_count(n);
+	tmp = malloc((len + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	return (ft_make(len, tmp, n));
 }
+ 
+/*
+#include<stdio.h>
+int	main()
+{
+	printf("FT_ITOA: %s %d", ft_itoa(-799), ft_count(-799));
+	return (0);
+}*/
